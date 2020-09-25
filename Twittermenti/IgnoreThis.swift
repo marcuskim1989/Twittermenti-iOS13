02557fc6cfb8d13a0fe069b986138e15b12b6bec
Marcus Kim <marcuskim1989@gmail.com>
2020-09-24 23:54:57 -0700

    test .gitignore

---
+++ Twittermenti/IgnoreThis.swift
@@ -0,0 +1,16 @@
+//
+//  IgnoreThis.swift
+//  Twittermenti
+//
+//  Created by Marcus Y. Kim on 9/24/20.
+//  Copyright © 2020 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct IgnoreThis {
+    
+    //let apiKey =
+    // let secretApiKey = 
+    
+}
